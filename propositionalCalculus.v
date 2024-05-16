7f16624ea85bed577434b508782bd7fada48af9d
piotr-lewandowski <lewandowskipiotr98@gmail.com>
2024-05-16 22:33:22 +0200

    Added scheffer and equivalence proof

--- propositionalCalculus.v
+++ propositionalCalculus.v
@@ -25,7 +25,7 @@ Notation "p <=> r" := (equiv p r) (at level 10, left associativity).
 Notation "s <- b" := (assign s b) (at level 0).  
 
 (* retrieveBool is helper function for evaluating propositions, 
-note that if there is no an suitable assignment in a given list
+note that if there is no suitable assignment in a given list
 we simply return false *)
 Fixpoint retrieveBool (s : propVar) (l : list assignment) : bool := 
   match l with
@@ -99,7 +99,7 @@ Fixpoint convertAssignmentsToFormula (l : list assignment) :=
   | (s <- b) :: [] => if b then # s else ¬ # s
   | (s <- b) :: tail => if b then # s ∧ (convertAssignmentsToFormula tail) 
                            else ¬ # s ∧ (convertAssignmentsToFormula tail)
-  end. 
+  end.
 
 Definition convertToDNF (p : proposition) : proposition := 
   let vars := removeDuplicates (getVars p) in 
@@ -126,4 +126,102 @@ Definition convertToCNF (p : proposition) : proposition :=
   negateProposition (convertToDNF (¬ p)).
 
 Definition prop3 : proposition := ¬ (# "x" ∧ # "y") <=> (¬ (¬ # "z" ∧ (# "x" <=> ¬ # "y"))).
-Eval compute in (convertToCNF prop3). 
+Eval compute in (convertToCNF prop3).
+
+Inductive scheffer :=
+  | scheffer_var : propVar -> scheffer
+  | scheffer_functor: scheffer -> scheffer -> scheffer.
+  
+Notation "$ s" := (scheffer_var s) (at level 1).
+Notation "p // r" := (scheffer_functor p r) (at level 5, left associativity).
+  
+Fixpoint evalS (s : scheffer) (l : list assignment): bool :=
+  match s with
+  | $ s => retrieveBool s l
+  | p // r => negb (andb (evalS p l) (evalS r l))
+  end.
+  
+Definition negS (r : scheffer) : scheffer := r // r.
+Definition andS (r q : scheffer) : scheffer := (q // r) // (q // r).
+Definition orS (r q : scheffer) : scheffer := negS (andS (negS r) (negS q)).
+Definition implS (r q : scheffer) : scheffer := orS (negS r) q.
+Definition equivS (r q : scheffer) : scheffer := andS (implS r q) (implS q r).
+  
+Fixpoint convertToScheffer (p : proposition) : scheffer :=
+  match p with
+  | # s => $ s
+  | ¬ r => negS (convertToScheffer r)
+  | r ∨ q => let rs := convertToScheffer r in let qs := convertToScheffer q in orS rs qs
+  | r ∧ q => let rs := convertToScheffer r in let qs := convertToScheffer q in andS rs qs
+  | r → q => let rs := convertToScheffer r in let qs := convertToScheffer q in implS rs qs
+  | r <=> q => let rs := convertToScheffer r in let qs := convertToScheffer q in equivS rs qs
+  end.
+  
+Theorem scheffer_universality : forall p : proposition, exists s : scheffer, forall l : list assignment, evalS s l = eval p l.
+Proof.
+  intros.
+  induction p.
+  (* constants *)
+  exists ($ s).
+  trivial.
+  (* negation *)
+  destruct IHp.
+  exists (negS x).
+  intro l.
+  simpl.
+  rewrite (H l).
+  auto.
+  destruct (eval p l).
+  trivial.
+  trivial.
+  (* conjunction *)
+  destruct IHp1, IHp2.
+  exists (andS x x0).
+  intro l.
+  simpl.
+  rewrite (H l).
+  rewrite (H0 l).
+  destruct (eval p1 l), (eval p2 l).
+  trivial.
+  trivial.
+  trivial.
+  trivial.
+  (* disjunction *)
+  destruct IHp1, IHp2.
+  exists (orS x x0).
+  intro l.
+  simpl.
+  rewrite (H l).
+  rewrite (H0 l).
+  destruct (eval p1 l), (eval p2 l).
+  trivial.
+  trivial.
+  trivial.
+  trivial.
+  (* implication *)
+  destruct IHp1, IHp2.
+  exists (implS x x0).
+  intro l.
+  simpl.
+  rewrite (H l).
+  rewrite (H0 l).
+  destruct (eval p1 l), (eval p2 l).
+  trivial.
+  trivial.
+  trivial.
+  trivial.
+  (* equivalence *)
+  destruct IHp1, IHp2.
+  exists (equivS x x0).
+  intro l.
+  simpl.
+  rewrite (H l).
+  rewrite (H0 l).
+  destruct (eval p1 l), (eval p2 l).
+  trivial.
+  trivial.
+  trivial.
+  trivial.
+Qed.
+  
+  
